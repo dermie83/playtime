febdab37f1580fdd9b0d2615e4326ea09b7bed58
dermie83 <dermiemadsen@gmail.com>
2024-02-08 07:55:05 +0000

    tdd user-model-store

--- src/models/mem/user-mem-store.js
+++ src/models/mem/user-mem-store.js
@@ -10,21 +10,25 @@ export const userMemStore = {
   async addUser(user) {
     user._id = v4();
     users.push(user);
-    console.log(`UserId ${  user._id}`);
+    console.log(`Added UserId ${  user._id}`);
     return user;
   },
 
   async getUserById(id) {
-    return users.find((user) => user._id === id);
+    let u = users.find((user) => user._id === id);
+    if (u === undefined) u = null;
+    return u;
   },
 
   async getUserByEmail(email) {
-    return users.find((user) => user.email === email);
+    let u = users.find((user) => user.email === email);
+    if (u === undefined) u = null;
+    return u;
   },
 
   async deleteUserById(id) {
     const index = users.findIndex((user) => user._id === id);
-    users.splice(index, 1);
+    if (index !== -1) users.splice(index, 1);
   },
 
   async deleteAll() {
--- test/user-model-test.js
+++ test/user-model-test.js
@@ -2,12 +2,17 @@ import { assert } from "chai";
 import { db } from "../src/models/db.js";
 import { maggie, testUsers } from "./fixtures.js";
 
-assert.equal(2, 2);
+// assert.equal(2, 2);
 
 suite("User API tests", () => {
 
   setup(async () => {
     db.init();
+    await db.userStore.deleteAll();
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testUsers[i] = await db.userStore.addUser(testUsers[i]);
+    }
   });
 
   test("create a user", async () => {
@@ -15,15 +20,66 @@ suite("User API tests", () => {
     assert.deepEqual(maggie, newUser)
   });
 
-  test("delete all users", async () => {
-    for (let i = 0; i < testUsers.length; i += 1) {
-      // eslint-disable-next-line no-await-in-loop
-      await db.userStore.addUser(testUsers[i]);
-    }
-    let returnedUsers = await db.userStore.getAllUsers();
-    assert.equal(returnedUsers.length, 3);
-    await db.userStore.deleteAll();
-    returnedUsers = await db.userStore.getAllUsers();
-    assert.equal(returnedUsers.length, 0);
+  // test("get a user - failures", async () => {
+  //   const noUserWithId = await db.userStore.getUserById("123");
+  //   console.log("Users id ",noUserWithId);
+  //   assert.isNull(noUserWithId);
+  //   const noUserWithEmail = await db.userStore.getUserByEmail("no@one.com");
+  //   console.log("Users email ",noUserWithEmail);
+  //   assert.isNull(noUserWithEmail);
+  // });
+
+  // test("get a user - bad params", async () => {
+  //   let nullUser = await db.userStore.getUserByEmail("");
+  //   console.log("Bad email param ", nullUser);
+  //   assert.isNull(nullUser);
+  //   nullUser = await db.userStore.getUserById("");
+  //   console.log("Bad userid 1 param ", nullUser);
+  //   assert.isNull(nullUser);
+  //   nullUser = await db.userStore.getUserById();
+  //   console.log("Bad userid 2 param ", nullUser);
+  //   assert.isNull(nullUser);
+  // });
+
+  // test("delete One User - fail", async () => {
+  //   await db.userStore.deleteUserById("bad-id");
+  //   const allUsers = await db.userStore.getAllUsers();
+  //   console.log("delete One User - Fail ", allUsers);
+  //   console.log("testuser count ", testUsers.length, "alluser count ", allUsers.length);
+  //   assert.equal(testUsers.length, allUsers.length);
+  // });
+
+  // test("delete all users", async () => {
+  //   let returnedUsers = await db.userStore.getAllUsers();
+  //   console.log("Total Returned Users ",returnedUsers.length);
+  //   assert.equal(returnedUsers.length, 3);
+  //   await db.userStore.deleteAll();
+  //   returnedUsers = await db.userStore.getAllUsers();
+  //   assert.equal(returnedUsers.length, 0);
+  // });
+
+  // test("get a user - success", async () => {
+  //   const user = await db.userStore.addUser(maggie);
+  //   console.log("Added User ",user);
+  //   const returnedUser1 = await db.userStore.getUserById(user._id);
+  //   console.log("Returned User ",returnedUser1);
+  //   assert.deepEqual(user, returnedUser1);
+  //   const returnedUser2 = await db.userStore.getUserByEmail(user.email);
+  //   assert.deepEqual(user, returnedUser2);
+  // });
+
+  test("delete One User - success", async () => {
+    // for (let i = 0; i < testUsers.length; i += 1) {
+    //   // eslint-disable-next-line no-await-in-loop
+    //   testUsers[i] = await db.userStore.addUser(testUsers[i]);
+    // }
+    await db.userStore.deleteUserById(testUsers[0]._id);
+    const returnedUsers = await db.userStore.getAllUsers();
+    console.log("Returned Users ",returnedUsers);
+    assert.equal(returnedUsers.length, testUsers.length - 1);
+    const deletedUser = await db.userStore.getUserById(testUsers[0]._id);
+    console.log("Deleted User Returned ",deletedUser);
+    assert.isNull(deletedUser);
   });
+
 });
